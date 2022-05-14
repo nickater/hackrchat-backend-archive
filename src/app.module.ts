25f86c3bf252a57386d1423a7e0fd206340c3bb4
Nick Ater <nick@atercloud.com>
2022-05-14 16:55:37 -0400

    added typerom

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from './typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRootAsync(typeOrmConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/typeorm.config.ts
@@ -0,0 +1,18 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+
+export const typeOrmConfig: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: (configService: ConfigService) => ({
+    type: 'postgres',
+    host: configService.get('DB_HOST'),
+    port: parseInt(configService.get('DB_PORT')),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+    entities: [User],
+    synchronize: true,
+  }),
+  inject: [ConfigService],
+};
